1f30e0da477ff8035c9affcfefa0f9aaf8b9a095
Carlos Valenzuela <carlosvalenzuelag93@gmail.com>
2025-02-14 10:42:34 +0100

    Cambio Grop por Team

---
+++ src/app/pages/new-team/new-team.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-team',
+  imports: [],
+  templateUrl: './new-team.component.html',
+  styleUrl: './new-team.component.css'
+})
+export class NewTeamComponent {
+
+}
