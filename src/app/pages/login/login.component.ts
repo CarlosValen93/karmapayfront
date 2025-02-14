1f7b2a2d40b36bf4a0c492e4596f5ac36a84e4e7
Carlos Valenzuela <carlosvalenzuelag93@gmail.com>
2025-02-14 13:38:43 +0100

    Maquetacion de loggin creda

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup
+
+  constructor() {
+  
+    this.loginForm = new FormGroup({
+      mail: new FormControl(),
+      password: new FormControl()
+    })
+  }
+
+  onSubmit(){
+
+  }
 
 }
