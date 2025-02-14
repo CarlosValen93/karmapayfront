7b56e9fdcfb930d314ac1eb5c4c84e74964eaee7
Lucrecia Correa <lucrecia.correa@hotmail.com>
2025-02-14 14:21:38 +0100

    hmtl basico header

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-<<<<<<< HEAD
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-=======
-import { provideHttpClient } from '@angular/common/http';
->>>>>>> CreateServices
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
   ]
 };
