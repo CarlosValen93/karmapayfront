daf11e842e31df38cffc549d8ce7ec95c41c3396
Gonzalo <belmonte86@gmail.com>
2025-02-14 18:46:29 +0100

    Routes.ts V1 done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,20 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { UserComponent } from './pages/user/user.component';
+import { TeamComponent } from './pages/team/team.component';
+import { ExpenseComponent } from './pages/expense/expense.component';
+import { NewExpenseComponent } from './pages/new-expense/new-expense.component';
+import { NewTeamComponent } from './pages/new-team/new-team.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent }
+    { path: '', pathMatch: 'full', redirectTo: '/home' },
+    { path: 'register', component: RegisterComponent},
+    { path: 'login', component: LoginComponent },
+    { path: 'team', component: TeamComponent },
+    { path: 'user', component: UserComponent },
+    { path: 'expense', component: ExpenseComponent },
+    { path: 'expense/new', component: NewExpenseComponent },
+    { path: 'team/new', component: NewTeamComponent },
+    { path: '*', redirectTo: '/home' } 
 ];
