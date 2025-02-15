934bbb27b69909ccbe5de78fe1fbc1d2266df099
Carlos Valenzuela <carlosvalenzuelag93@gmail.com>
2025-02-15 11:59:58 +0100

    Page register maquetada

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registroForm: FormGroup;
+  usersService = inject(UsersService)
+  constructor() {
+    this.registroForm = new FormGroup({
+      username: new FormControl(),
+      mail: new FormControl(),
+      password: new FormControl(),
+      checkPassword: new FormControl()
+    });
+  }
+async onSubmit(){
+  const user = await this.usersService.register();
 
+}
 }
