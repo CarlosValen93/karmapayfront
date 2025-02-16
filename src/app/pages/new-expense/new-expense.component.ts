0e1c2026d1ff6f2ba2c8da671c3042c9ccf4f0b9
Carlos Valenzuela <carlosvalenzuelag93@gmail.com>
2025-02-16 11:34:20 +0100

    New expense y New team hecho

--- src/app/pages/new-expense/new-expense.component.ts
+++ src/app/pages/new-expense/new-expense.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ExpensesService } from '../../services/expenses.service';
 
 @Component({
   selector: 'app-new-expense',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-expense.component.html',
   styleUrl: './new-expense.component.css'
 })
 export class NewExpenseComponent {
+  registerForm : FormGroup;
+  expensesService = inject(ExpensesService)
+   constructor() {
+      this.registerForm = new FormGroup({
+       name: new FormControl(),
+      amount: new FormControl(),
+       assignation: new FormControl()
+       })
+}
+onSubmit(){
 
+}
 }
