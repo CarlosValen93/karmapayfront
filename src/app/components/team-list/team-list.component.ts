85ab46cd1e6f0e95a7f5ccf8894abef21abff652
Lucrecia Correa <lucrecia.correa@hotmail.com>
2025-02-17 15:18:45 +0100

    ts.team list

--- src/app/components/team-list/team-list.component.ts
+++ src/app/components/team-list/team-list.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { TeamsService } from './../../services/teams.service';
+import { Component, inject } from '@angular/core';
 import { TeamCardComponent } from '../team-card/team-card.component';
+import { ITeam } from '../../interface/team.interface';
 
 @Component({
   selector: 'app-team-list',
@@ -8,5 +10,10 @@ import { TeamCardComponent } from '../team-card/team-card.component';
   styleUrl: './team-list.component.css'
 })
 export class TeamListComponent {
-
-}
+  /*   arrTeams: ITeam[] = [];
+    teamsService: inject(TeamsService);
+  }
+  
+  ngOnInit() {
+  */
+} 
