3475c77ec66008465db3c121a0a1b4fb4d3f3cdc
Gonzalo <belmonte86@gmail.com>
2025-02-17 20:11:52 +0100

    Funciones creadas en Expense /List/Card, User /List/Card, Team

--- src/app/pages/expense/expense.component.ts
+++ src/app/pages/expense/expense.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, Input } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
 import { ExpensesService } from '../../services/expenses.service';
 import { IExpense } from '../../interface/expense.interface';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-expense',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './expense.component.html',
   styleUrl: './expense.component.css'
 })
@@ -15,8 +16,50 @@ export class ExpenseComponent {
   router =inject(Router);
   expense!: IExpense
 
-ngOnInit(){
+  async ngOnInit() {
+    try {
+      let id: number = Number(this.idExpense);
+      let response = await this.expensesService.getById(id);
+      if (response) {
+        this.expense = response;
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "El Id de gasto no existe",
+        });
+        this.router.navigate(['/home']);
+      }
+    } catch (error) {
+      //  Swal.fire({
+       //   icon: "error",
+       //   title: "Oops...",
+       //   text: "Error al obtener el gasto",
+       // });
+       // this.router.navigate(['/home']);
+    }
+  }
 
-}
+  async ngDeleteExpense() {
+    try {
+      let id: number = Number(this.idExpense);
+      let response = await this.expensesService.delete(id);
+      Swal.fire({
+        icon: "success",
+        title: "Borrado!",
+        text: "El gasto ha sido borrado",
+      });
+        this.router.navigate(['/team/:idTeam']);
+    } catch (error) {
+      Swal.fire({
+       icon: "error",
+       title: "Oops...",
+       text: "Error al borrar el gasto",
+    });
+    }
+  }
+
+  ngChangeInfo(){
+  }
 
 }
