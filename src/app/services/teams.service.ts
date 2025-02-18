ca064ef9c0659740e5eae7c3fe722e9459411817
Lucrecia Correa <lucrecia.correa@hotmail.com>
2025-02-18 13:00:42 +0100

    Create select-category components.

--- src/app/services/teams.service.ts
+++ src/app/services/teams.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject,Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { ITeam } from '../interface/team.interface';
 import { firstValueFrom } from 'rxjs';
 import { environment } from '../environments/environment.development';
@@ -12,44 +12,44 @@ export class TeamsService {
   private httpClient = inject(HttpClient)
   private baseUrl = `${environment.apiUrl}/api/teams`;
 
-  getAll(){
+  getAll() {
     return firstValueFrom(
       this.httpClient.get<ITeam[]>(`${this.baseUrl}`)
     );
 
-}
-getByName(name: string) {
-  return firstValueFrom(
-    this.httpClient.get<ITeam[]>(`${this.baseUrl}/name/${name}`)
-  );
-}
-getById(id: number) {
-  return firstValueFrom(
-    this.httpClient.get<ITeam | null>(`${this.baseUrl}/${id}`)
-  ).then(team => {
-    if (!team) {
-      throw new Error(`ID ${id} no encontrado`);
-    }
-    return team;
-  }).catch(error => {
-    throw new Error(`Error al obtener el equipo: ${error.message}`);
-  });
-}
-
-updateById(id: number, body: Partial<UserBody>): Promise<ITeam> {
-  if (Object.keys(body).length === 0) {
-    return Promise.reject(new Error("No has actualizado ningún campo"));
+  }
+  getByName(name: string) {
+    return firstValueFrom(
+      this.httpClient.get<ITeam[]>(`${this.baseUrl}/name/${name}`)
+    );
+  }
+  getById(id: number) {
+    return firstValueFrom(
+      this.httpClient.get<ITeam | null>(`${this.baseUrl}/${id}`)
+    ).then(team => {
+      if (!team) {
+        throw new Error(`ID ${id} no encontrado`);
+      }
+      return team;
+    }).catch(error => {
+      throw new Error(`Error al obtener el equipo: ${error.message}`);
+    });
   }
 
-  return firstValueFrom(
-    this.httpClient.put<ITeam>(`${this.baseUrl}/update/${id}`, body)
-  ).catch(error => {
-    throw new Error(`Error al actualizar el equipo con ID ${id}: ${error.message}`);
-  });
-}
+  updateById(id: number, body: Partial<UserBody>): Promise<ITeam> {
+    if (Object.keys(body).length === 0) {
+      return Promise.reject(new Error("No has actualizado ningún campo"));
+    }
+
+    return firstValueFrom(
+      this.httpClient.put<ITeam>(`${this.baseUrl}/update/${id}`, body)
+    ).catch(error => {
+      throw new Error(`Error al actualizar el equipo con ID ${id}: ${error.message}`);
+    });
+  }
 
 
-  createTeam(body:UserBody){
+  createTeam(body: UserBody) {
     return firstValueFrom(
       this.httpClient.post<ITeam>(`${this.baseUrl}/create`, body)
     )
