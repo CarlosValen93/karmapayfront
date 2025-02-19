dc80d3e30a67194e589830835ebd4017476e820f
Carlos Valenzuela <carlosvalenzuelag93@gmail.com>
2025-02-19 14:11:01 +0100

    new expenses

--- src/app/pages/new-expense/new-expense.component.ts
+++ src/app/pages/new-expense/new-expense.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ExpensesService } from '../../services/expenses.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-new-expense',
@@ -13,16 +14,54 @@ export class NewExpenseComponent {
   expensesService = inject(ExpensesService)
    constructor() {
       this.registerForm = new FormGroup({
-       name: new FormControl(),
-      amount: new FormControl()
+        name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+        amount: new FormControl('', [Validators.required, Validators.min(0.5), Validators.max(1000000), Validators.pattern("^[0-9]*$")])
+      });
        //assignation: new FormControl()
-       })
+      
 }
-  async onSubmit(){
+async onSubmit() {
+  if (this.registerForm.invalid) {
+    Swal.fire({
+      title: 'Error',
+      text: 'Los datos ingresados no son válidos. Verifica los campos.',
+      icon: 'error',
+      confirmButtonText: 'Aceptar'
+    });
+    return;
+  }
+
   const expenseBody = {
     name: this.registerForm.value.name,
     amount: this.registerForm.value.amount
   };
-  const user = this.expensesService.add(expenseBody);
+
+  try {
+    const newExpense = await this.expensesService.add(expenseBody);
+
+    Swal.fire({
+      title: '¡Gasto añadido!',
+      text: 'Se ha añadido tu gasto al grupo',
+      icon: 'success',
+      confirmButtonText: 'Aceptar'
+    });
+
+    this.registerForm.reset();
+  } catch (error) {
+    Swal.fire({
+      title: 'Error',
+      text: 'No se pudo agregar el gasto. Intenta de nuevo.',
+      icon: 'error',
+      confirmButtonText: 'Cerrar'
+    });
+    console.error(error);
+  }
 }
+
+checkErrorField(field: string, error: string): boolean {
+  const control = this.registerForm.get(field);
+  return control ? control.hasError(error) && control.touched : false;
 }
+
+}
+
