269971d2b31bdffc09940729a9e7db4578fd187a
AlexCastetPedrosa <alexcasteto03@gmail.com>
2025-02-19 13:39:52 +0100

    Update-Expense Created and implemented v1

---
+++ src/app/pages/expense/update-expense/update-expense.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ExpensesService } from '../../../services/expenses.service';
+
+@Component({
+  selector: 'app-update-expense',
+  imports: [ReactiveFormsModule],
+  templateUrl: './update-expense.component.html',
+  styleUrl: './update-expense.component.css'
+})
+export class UpdateExpenseComponent {
+    @Input() idExpense: number=0;
+  registerForm : FormGroup;
+  expensesService = inject(ExpensesService)
+   constructor() {
+      this.registerForm = new FormGroup({
+       name: new FormControl(),
+      amount: new FormControl()
+       //assignation: new FormControl()
+       })
+}
+async onSubmit(){
+  const expenseBody = {
+    name: this.registerForm.value.name,
+    amount: this.registerForm.value.amount
+  };
+  const user = this.expensesService.update(this.idExpense,expenseBody);
+}
+}
