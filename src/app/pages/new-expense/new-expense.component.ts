13abe6e68f9761c7b6e3ca73a99acb5ed750ac85
Cristina Roman <cristinaroman05.cr@gmail.com>
2025-02-20 15:13:24 +0100

    intento de assignation

--- src/app/pages/new-expense/new-expense.component.ts
+++ src/app/pages/new-expense/new-expense.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject, Input } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ExpensesService } from '../../services/expenses.service';
 import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { UsersService } from '../../services/users.service';
+import { IUser } from '../../interface/user.interface';
 
 @Component({
   selector: 'app-new-expense',
@@ -12,75 +14,92 @@ import { Location } from '@angular/common';
   styleUrl: './new-expense.component.css'
 })
 export class NewExpenseComponent {
-  @Input() idTeam: number=0;
+  @Input() idTeam: number = 0;
   router = inject(Router);
   location = inject(Location);
-
-
-  registerForm : FormGroup;
+  registerForm: FormGroup;
   expensesService = inject(ExpensesService)
-   constructor() {
-      this.registerForm = new FormGroup({
-        name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-        amount: new FormControl('', [Validators.required, Validators.min(0.5), Validators.max(1000000), Validators.pattern("^[0-9]*$")])
-      });
-       //assignation: new FormControl()
-      
-}
+  userService = inject(UsersService)
+  arrUsersTeam: IUser[] = []
+  constructor() {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      amount: new FormControl('', [Validators.required, Validators.min(0.5), Validators.max(1000000), Validators.pattern("^[0-9]*$")]),
+      assignations: new FormArray<FormGroup>([])
+    });
+    //assignation: new FormControl()
 
-async onSubmit() {
-  if (this.registerForm.invalid) {
-      Swal.fire({
-          title: 'Error',
-          text: 'Por favor, completa todos los campos correctamente.',
-          icon: 'error',
-          confirmButtonText: 'Revisar'
-      });
-      return;
   }
 
-  const expenseBody = {
-    name: this.registerForm.value.name,
-    amount: this.registerForm.value.amount,
-    teamId: this.idTeam
-  };
-console.log(expenseBody);
+  async ngOnInit() {
+    this.arrUsersTeam = await this.userService.getByIdGroup(this.idTeam)
+    for (let user of this.arrUsersTeam) {
+      const assignationUser = new FormGroup({
+        Username: new FormControl(user.Username),
+        Assignation: new FormControl()
+      });
+      this.assignations.push(assignationUser)
+    }
+  }
 
-  try {
+  get assignations() {
+    return this.registerForm.get("assignations") as FormArray
+  }
+
+  async onSubmit() {
+    console.log(this.registerForm.value)
+    if (this.registerForm.invalid) {
+      Swal.fire({
+        title: 'Error',
+        text: 'Por favor, completa todos los campos correctamente.',
+        icon: 'error',
+        confirmButtonText: 'Revisar'
+      });
+      return;
+    }
+
+    const expenseBody = {
+      name: this.registerForm.value.name,
+      amount: this.registerForm.value.amount,
+      teamId: this.idTeam
+    };
+    console.log(expenseBody);
+
+    try {
       await this.expensesService.add(expenseBody);
 
       Swal.fire({
-          title: '¡Gasto agregado!',
-          text: `El gasto ha sido registrado correctamente.`,
-          icon: 'success',
-          confirmButtonText: 'Aceptar'
+        title: '¡Gasto agregado!',
+        text: `El gasto ha sido registrado correctamente.`,
+        icon: 'success',
+        confirmButtonText: 'Aceptar'
       });
 
       this.registerForm.reset();
       this.router.navigate(['/team/' + this.idTeam]);
 
-   } catch (error: any) {
-    console.error(error);
+    } catch (error: any) {
+      console.error(error);
 
-    Swal.fire({
+      Swal.fire({
         title: 'Error',
         text: error?.error?.message || 'No se pudo agregar el gasto. Intenta de nuevo.',
         icon: 'error',
         confirmButtonText: 'Cerrar'
-    });
-}
-}
+      });
+    }
+  }
 
 
 
-checkErrorField(field: string, error: string): boolean {
-  const control = this.registerForm.get(field);
-  return control ? control.hasError(error) && control.touched : false;
-}
+  checkErrorField(field: string, error: string): boolean {
+    const control = this.registerForm.get(field);
+    return control ? control.hasError(error) && control.touched : false;
+  }
 
-goBack() {
-  this.location.back();
-}
+  goBack() {
+    this.location.back();
+  }
 
 }
 
