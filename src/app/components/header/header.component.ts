afe2a7028e0cc1d94fe310880dd7fe75bb1f68c6
Cristina Roman <cristinaroman05.cr@gmail.com>
2025-02-22 11:31:24 +0100

    arreglado error del token en el header y añadido redirigir cuando haces logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { environment } from './../../environments/environment';
 import { CustomPayload } from './../../guards/auth.guard';
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { UsersService } from '../../services/users.service';
 import { jwtDecode } from 'jwt-decode';
 import { ButtonComponent } from "../button/button.component";
@@ -18,16 +18,21 @@ export class HeaderComponent {
   usersService = inject(UsersService);
   userId!: number;
   mail: string = ""
+  router = inject(Router)
 
   async ngOnInit() {
-    const token = localStorage.getItem(environment.tokenName)!;
-    const payload = jwtDecode<CustomPayload>(token);
-    this.userId = payload.userId;
-    const user = await this.usersService.getById(this.userId)
-    this.mail = user?.Mail!
+    if (this.usersService.isLogged()) {
+      const token = localStorage.getItem(environment.tokenName)!;
+      const payload = jwtDecode<CustomPayload>(token);
+      this.userId = payload.userId;
+
+      const user = await this.usersService.getById(this.userId)
+      this.mail = user?.Mail!
+    }
 
   }
   async logout() {
     await this.usersService.logout()
+    this.router.navigateByUrl('/home')
   }
 }
