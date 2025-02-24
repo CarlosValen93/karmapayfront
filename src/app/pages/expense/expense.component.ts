c1ba5df7e710c681c93d244a0272f434520d6951
Cristina Roman <cristinaroman05.cr@gmail.com>
2025-02-24 11:20:27 +0100

    fix

--- src/app/pages/expense/expense.component.ts
+++ src/app/pages/expense/expense.component.ts
@@ -8,7 +8,7 @@ import { ButtonComponent } from "../../components/button/button.component";
 
 @Component({
   selector: 'app-expense',
-  imports: [RouterLink, DatePipe, ButtonComponent],
+  imports: [DatePipe, ButtonComponent],
   templateUrl: './expense.component.html',
   styleUrl: './expense.component.css'
 })
